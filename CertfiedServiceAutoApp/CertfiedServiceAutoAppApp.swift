827fed023770cad050f6c13ba46fac02566fc6cb
BlakeT <BlakeTswift@gmail.com>
2025-10-30 13:34:11 -0600

    Initial Commit

---
+++ CertfiedServiceAutoApp/CertfiedServiceAutoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertfiedServiceAutoAppApp.swift
+//  CertfiedServiceAutoApp
+//
+//  Created by Trytten, Blake - Student on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CertfiedServiceAutoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CertfiedServiceAutoAppTests/CertfiedServiceAutoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CertfiedServiceAutoAppTests.swift
+//  CertfiedServiceAutoAppTests
+//
+//  Created by Trytten, Blake - Student on 10/30/25.
+//
+
+import Testing
+@testable import CertfiedServiceAutoApp
+
+struct CertfiedServiceAutoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
