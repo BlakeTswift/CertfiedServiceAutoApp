3ffa3e0e1de2f5a2ceefb269d43df2a2685a9a10
BlakeT <BlakeTswift@gmail.com>
2025-10-31 13:11:47 -0600

    This app is not even close but I thought I may as well comit and push it

--- CertfiedServiceAutoApp/ContentView.swift
+++ CertfiedServiceAutoApp/ContentView.swift
@@ -6,19 +6,78 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var Scratches: [Int] = [0,0]
+    @State var ResivedData: [String] = [""]
+    @State var RFcondition: Int = 0
+    @State var InformationOnCar: [String] =
+    ["",""]
+    
+    @State var SelectedDate = Date()
+    
     var body: some View {
+        Text(InformationOnCar.description)
+            .font(.largeTitle)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            DataEntryCard(InformationOnCar: $InformationOnCar, TextName: "Name")
+            HStack(spacing: 20){
+                DatePicker("Date:", selection: $SelectedDate,displayedComponents: .date)
+                    .fixedSize()
+                    .datePickerStyle(.compact)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                HStack {
+                    Text("RF")
+                        .font(.largeTitle)
+                        .bold()
+                    Button{
+                        RFcondition += 1
+                        if RFcondition > 3 {
+                            RFcondition = 0
+                        }
+                    }
+                    label:{
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Condition(Rfconditon: RFcondition))
+                    }
+                }
+                .padding(10)
+                .border(Color.black)
+                .frame(maxWidth: 500, maxHeight: 75)
+                
+            }
         }
-        .padding()
+    }
+    func Condition(Rfconditon: Int) -> Color {
+        
+        
+        if Rfconditon == 1 {
+            return .green
+        }
+        if Rfconditon == 2 {
+            return .yellow
+        }
+        if Rfconditon == 3 {
+            return .red
+        }else{
+            return .gray
+        }
     }
 }
-
+func dataEntryCard(want: String) {
+    @Binding var ResivedData: [String]
+    var data: String
+    
+//    ForEach (0..<ResivedData) {data in
+//        HStack {
+//            Text(want)
+//            TextField(want, text: $data)
+//                .padding()
+//        }
+//    }
+    
+}
 #Preview {
     ContentView()
 }
